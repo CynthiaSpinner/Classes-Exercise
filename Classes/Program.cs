b4fb419f164175e4c0a7ba3362daf1af7d45542b
Cynthia <CynthiaSpinner@gmail.com>
2024-04-03 15:41:54 -0500

    completed classes exercise 1

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Chevy";
+            myCar.Model = "Blazer";
+            myCar.Year = 1998;
+
+            var toyota = new Car("Toyota", "Camry", 2001);
+
+            var rover = new Car()
+            {
+                Make = "Land Rover",
+                Model = "Ranger Rover Sport HSE",
+                Year = 2013
+            };
+
+            
+
+            var carList = new List<Car>() { myCar, toyota, rover};
+
+            
+            foreach (var vehicle in carList) 
+            {
+                Console.WriteLine($"{vehicle.Make} {vehicle.Model} {vehicle.Year}");
+            }
         }
     }
 }
